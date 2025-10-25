9795872c84533ed92c8110431399ca8a175b6842
tn69code <mkyawkyaw867@gmail.com>
2025-10-25 07:12:57 +0700

    Update udpagn1.sh

--- udpagn1.sh
+++ udpagn1.sh
@@ -413,4 +413,4 @@ EOF
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <span :class="['px-2 inline-flex text-xs leading-5 font-semibold rounded-full', 
                                                      user.active ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800']">
-                                            {{ user.active
+                                            {{ user.active }}
